7588e1c70325fe02c49f38533a4ca2dee9fa623c
SonalPawar2509 <makhare.sonal@gmail.com>
2022-01-18 17:25:36 +0100

    first commit

---
+++ cypress/integration/helpers/loginpagelocators.ts
@@ -0,0 +1,39 @@
+export class loginpagelocators{
+
+    static loginAsUser() {
+        cy.acceptCookies()
+    
+    }
+
+    static userNameTxtbox = '#name'
+
+    static emailTxtbox = '#email'
+
+    static userPassTxtBox = '#password'
+
+    static checkboxTerms = '.mr-checkbox-1__check'
+
+    static submitBtn = '[data-testid=mr-form-signup-btn-start-1]'
+
+    static submitBtnTxt = 'Get started now'
+
+    static txtCheckYourEmail = 'Überprüfen Sie Ihre E-Mail'
+
+    static emailErrorMessage = 'Tut uns Leid, diese E-Mail ist bereits registriert'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
